d00625bda5f106f0901b12862671ea7a60196aed
celloza <dupreez.marcel@gmail.com>
2024-12-14 12:40:31 +0000

    Fix build

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -6,7 +6,7 @@
 $modules = @("Pester", "PSScriptAnalyzer")
 
 # Automatically add missing dependencies
-$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\ucm-psmodule\ucm-psmodule.psd1"
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\ucm-psmodule.psd1"
 foreach ($dependency in $data.RequiredModules) {
     if ($dependency -is [string]) {
         if ($modules -contains $dependency) { continue }
