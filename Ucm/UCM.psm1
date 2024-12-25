15f9f129389cecae85f04a6a32240467a3a966c4
celloza <dupreez.marcel@gmail.com>
2024-12-25 23:22:51 +0000

    Fix cmdlet exports

--- Ucm/UCM.psm1
+++ Ucm/UCM.psm1
@@ -1,6 +1,6 @@
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\UCM\functions\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\UCM\internal\*.ps1 -ErrorAction SilentlyContinue )
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\UCM\functions\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\UCM\internal\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 
 #Dot source the files
 foreach($import in @($Public + $Private))
